81c1a79a460418c18998bf88daf32af4f6808940
eddDevDz95 <houedd608@gmail.com>
2024-10-20 13:35:49 +0100

    new commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <script  data-cfasync="false" src="//affordspoonsgray.com/5124eefbaa72ac6102cfa1f1d50092bc/invoke.js"></script>
-        <script type='text/javascript' src='//affordspoonsgray.com/2a/86/2b/2a862b1af2220cccdea340b42f065a42.js'></script>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark" 
@@ -34,6 +33,8 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <Analytics />
+        <Script  data-cfasync="false" src="//affordspoonsgray.com/5124eefbaa72ac6102cfa1f1d50092bc/invoke.js" />
+        <Script type='text/javascript' src='//affordspoonsgray.com/2a/86/2b/2a862b1af2220cccdea340b42f065a42.js' />
       </body>
     </html>
   );
